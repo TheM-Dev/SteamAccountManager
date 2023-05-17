c41e10ce0788bbda0e9f79bef4543517bc590aed
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-05-17 11:02:35 +0200

    Added an example account file
    This file displays how other account files should be set up

---
+++ src/accounts/example.js
@@ -0,0 +1,15 @@
+module.exports = {
+    login: 'login',
+    password: 'password',
+
+    sharedSecret: 'sharedSecret',
+    identitySecret: 'identitySecret',
+
+    masterID: 'masterSteamID64',
+
+    respondToOffers: true,
+    respondToMessages: true,
+
+    idle: true,
+    games: [ 730 ]
+}
