030e58451b85b1a81626e57efabf1d12589e94ff
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-05-17 12:03:39 +0200

    Basic config for entire app

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    currency: 'PLN',
+    steam: {
+        machineName: 'SteamAccountManager',
+        //Displayed on Steam games page (web browser) as your PC name, cna be set to anything, doesnt matter
+    }
+}
