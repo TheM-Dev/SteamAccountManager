7c76af1941a69b312a5591c265424796a3de4170
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-05-17 12:04:03 +0200

    Added first language
    App will allow you to choose between different languages!

---
+++ src/dict/english.js
@@ -0,0 +1,24 @@
+module.exports = {
+    steam: {
+        gameInviteReceived: (client, nickname, message) => {
+            client.chat.sendFriendMessage(
+                message.steamid_friend,
+                `/me Hey ${nickname}! Please, do not invite me to the game!`
+            );
+            return client.chat.sendFriendMessage(
+                message.steamid_friend,
+                `/me I'm using SteamAccountManager, idling games hours AND/OR collectible Steam cards!`
+            )
+        },
+        friendMessageReceived: (client, nickname, message) => {
+            client.chat.sendFriendMessage(
+                message.steamid_friend,
+                `Hello ${nickname}! Thanks for the message :steamhappy:`
+            );
+            return client.chat.sendFriendMessage(
+                message.steamid_friend,
+                `Unfortunately, I cannot respond to your message, because I'm currently using SteamAccountManager, idling hours AND/OR collectible Steam cards!`
+            );
+        }
+    }
+}
