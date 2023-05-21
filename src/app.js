1676cfe3411cf966caf76ddc3d7d9a71308ea8c2
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-05-21 14:11:30 +0200

    start of main app file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 //Controls the overall of the app
 //Turns on every enabled component
-const web = require('./components/dashboard/server');
-const webServer = new web();
+const expressServer = require('./components/dashboard/server');
+const server = new expressServer();
+const log = require('./utils/log');
+
+server.app.post('/statuses/process/update', server.loggingMiddleware, server.jsonParser, (req, res) => {
+    log(1, 'web_dashboard', `Process is using ${req.body.memory}MB of memory and ${req.body.cpu}% of CPU!`)
+    res.sendStatus(200);
+});
