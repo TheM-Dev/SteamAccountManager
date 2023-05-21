0e73cb97a14c9d4093f9556b7464d0ee1ff2440f
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-05-21 14:14:08 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const expressServer = require('./components/dashboard/server');
 const server = new expressServer();
 const log = require('./utils/log');
 
-server.app.post('/statuses/process/update', server.loggingMiddleware, server.jsonParser, (req, res) => {
-    log(1, 'web_dashboard', `Process is using ${req.body.memory}MB of memory and ${req.body.cpu}% of CPU!`)
-    res.sendStatus(200);
+server.app.post('/statuses/process/update', server.loggingMiddleware, (req, res) => {
+    log(1, 'web_dashboard', req)
+    res.sendStatus(200)
 });
