71865537b000a806a80fc411c1a9d03fcdd1b9ed
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-05-21 17:17:49 +0200

    rewritten file to work with sockets

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
+const log = require('./utils/log');
 //Controls the overall of the app
 //Turns on every enabled component
-const expressServer = require('./components/dashboard/server');
-const server = new expressServer();
-const log = require('./utils/log');
 const loadAccounts = require('./components/steam/utils/loadAccounts');
 let accounts = [];
 (async () => { accounts = await loadAccounts(); })();
 
+const expressServer = require('./components/api/server');
+const server = new expressServer();
 server.app.post('/statuses/process/update', server.loggingMiddleware, (req, res) => {
     let { cpu, memory } = req.body;
     res.sendStatus(200)
 });
-
 server.app.get('/api/accounts', server.loggingMiddleware, (req, res) => {
     res.json(accounts);
 });
+
+const socketServer = require('./components/sockets/socket');
+const socket = new socketServer();
